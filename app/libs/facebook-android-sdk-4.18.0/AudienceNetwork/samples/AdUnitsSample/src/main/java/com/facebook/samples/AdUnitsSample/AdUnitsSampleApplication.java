b8295fb34ded50f0d9faffaf7b5b3ee79bdbfa7d
Deepak Shukla <codingbrains17@gmail.com>
2017-05-23 23:12:56 +0530

    initial code

---
+++ app/libs/facebook-android-sdk-4.18.0/AudienceNetwork/samples/AdUnitsSample/src/main/java/com/facebook/samples/AdUnitsSample/AdUnitsSampleApplication.java
@@ -0,0 +1,26 @@
+package com.facebook.samples.AdUnitsSample;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+import com.facebook.samples.ads.debugsettings.DebugSettings;
+
+public class AdUnitsSampleApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DebugSettings.initialize(this);
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
+    }
+}
