4de9de0ebd0a1ac040124491cb2f694888e4781d
Saurabh Singh <codingbrains46@gmail.com>
2017-06-13 22:49:18 +0530

    Changes in SignUp and Social Activity.

--- app/src/main/java/com/instamelody/instamelody/SocialActivity.java
+++ app/src/main/java/com/instamelody/instamelody/SocialActivity.java
@@ -1,39 +1,68 @@
 package com.instamelody.instamelody;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.annotation.NonNull;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.Switch;
+import android.widget.TabHost;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.share.Sharer;
 import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareDialog;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.plus.PlusOneButton;
-import com.google.android.gms.plus.PlusShare;
+import com.instamelody.instamelody.Parse.ParseContents;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 import io.fabric.sdk.android.Fabric;
+
+import static android.R.attr.description;
+import static android.R.attr.id;
+import static android.R.attr.negativeButtonText;
 
 public class SocialActivity extends AppCompatActivity {
 
@@ -46,45 +75,18 @@ public class SocialActivity extends AppCompatActivity {
     static String TWITTER_CONSUMER_KEY = "HPEUPWqatYYqdX2BXXZCwhRa3";
     static String TWITTER_CONSUMER_SECRET = "INlgRJqcVyxZe8tzfDhBZ0kYONTlWBY5NO8akXcnzVhERWL67I";
     TwitterAuthClient client;
-    GoogleApiClient mGoogleApiClient;
-    private static final int RC_SIGN_IN = 0;
-    SignInButton googleSignIn;
-    PlusOneButton plus_one_button;
-    private static final int PLUS_ONE_REQUEST_CODE = 0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
 
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                        // connection failed, should be handled
-                    }
-                })
-                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
-                .build();
-
-
-//        mPlusOneButton = (PlusOneButton) findViewById(R.id.plus_one_button);
-
-
         switchFb = (Switch) findViewById(R.id.switchFb);
         switchTwitter = (Switch) findViewById(R.id.switchTwitter);
         switchSoundCloud = (Switch) findViewById(R.id.switchSoundCloud);
         switchGoogle = (Switch) findViewById(R.id.switchGoogle);
-        googleSignIn = (SignInButton) findViewById(R.id.googleSignIn);
         SharedPreferences loginSharedPref1 = this.getSharedPreferences("Url_recording", MODE_PRIVATE);
         fetchRecordingUrl = loginSharedPref1.getString("Recording_url", null);
-        plus_one_button = (PlusOneButton) findViewById(R.id.plus_one_button);
 //        new URLShort().execute();
 
 
@@ -145,46 +147,7 @@ public class SocialActivity extends AppCompatActivity {
                 alert11.show();
             }
         });
-
-        switchGoogle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-//                startActivityForResult(signInIntent, RC_SIGN_IN);
-//                googleShare();
-                /*googleSignIn.setVisibility(View.VISIBLE);
-                googleSignIn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        signIn();
-
-                    }
-                });*/
-
-
-                plus_one_button.setVisibility(View.VISIBLE);
-                plus_one_button.setEnabled(true);
-                plus_one_button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent shareIntent = new PlusShare.Builder(SocialActivity.this)
-                                .setType("text/plain")
-                                .setText("Welcome to the Google+ platform.")
-                                .setContentUrl(Uri.parse(fetchRecordingUrl))
-                                .getIntent();
-
-                        startActivityForResult(shareIntent, 0);
-                    }
-                });
-
-
-            }
-        });
-
-
     }
-
 
     public void FbShare() {
         callbackManager = CallbackManager.Factory.create();
@@ -224,12 +187,9 @@ public class SocialActivity extends AppCompatActivity {
         if (requestCode == TwitterAuthConfig.DEFAULT_AUTH_REQUEST_CODE) {
             //  twitter related handling
             client.onActivityResult(requestCode, resultCode, data);
-        } else if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
         } else {
-            if (callbackManager.onActivityResult(requestCode, resultCode, data))
-                return;
+            // facebook related handling
+            callbackManager.onActivityResult(requestCode, resultCode, data);
         }
     }
 
@@ -244,12 +204,6 @@ public class SocialActivity extends AppCompatActivity {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
         Fabric.with(this, new TwitterCore(authConfig), new TweetComposer());
 
-        /*Bundle bundle = getIntent().getExtras().getBundle(SHARE_DATA);
-        String description = bundle.getString(SHARE_DESCRIPTION);
-        String title = bundle.getString(SHARE_TITLE);
-        String picture = bundle.getString(SHARE_PICTURE_LINK);
-        String link = bundle.getString(SHARE_LINK);*/
-
         TweetComposer.Builder builder = null;
 
 
@@ -262,56 +216,4 @@ public class SocialActivity extends AppCompatActivity {
         builder.show();
     }
 
-    public void googleShare() {
-
-        /*GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleSignInOptions.getAccount();*/
-
-
-        /*Intent shareIntent = new PlusShare.Builder(SocialActivity.this)
-                .setText("Android Testing for Google+")
-                .setType("text/plain")
-                .setContentUrl(
-                        Uri.parse(fetchRecordingUrl))
-                .getIntent()
-                .setPackage("com.google.android.apps.plus");
-        startActivityForResult(shareIntent, 0);*/
-
-
-
-
-
-
-        /*mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
-                .build();*/
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-
-    private void handleSignInResult(GoogleSignInResult result) {
-//        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            Toast.makeText(this, "" + acct.getDisplayName(), Toast.LENGTH_SHORT).show();
-//            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
-//            updateUI(true);
-        } else {
-            // Signed out, show unauthenticated UI.
-//            updateUI(false);
-        }
-    }
-
-
-    protected void onResume() {
-        super.onResume();
-        // Refresh the state of the +1 button each time the activity receives focus.
-        plus_one_button.initialize(fetchRecordingUrl, PLUS_ONE_REQUEST_CODE);
-    }
 }
