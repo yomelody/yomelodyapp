c3ed327d9b674fb36d6a5378678c4611ff9da382
CBPC-44\ADMIN <codingbrains23@gmail.com>
2017-06-24 04:21:18 +0530

    Ajay changes

--- app/src/main/java/com/instamelody/instamelody/Adapters/ActivityCardAdapter.java
+++ app/src/main/java/com/instamelody/instamelody/Adapters/ActivityCardAdapter.java
@@ -1,5 +1,6 @@
 package com.instamelody.instamelody.Adapters;
 
+import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -11,7 +12,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.instamelody.instamelody.Models.ActivityModel;
+import com.instamelody.instamelody.Models.MelodyCard;
 import com.instamelody.instamelody.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -19,50 +22,68 @@ import java.util.ArrayList;
  * Created by Saurabh Singh on 12/21/2016.
  */
 
-public class ActivityCardAdapter extends RecyclerView.Adapter<ActivityCardAdapter.MyViewHolder>{
-    private ArrayList<ActivityModel> dataset;
+public class ActivityCardAdapter extends RecyclerView.Adapter<ActivityCardAdapter.MyViewHolder> {
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
-        TextView tvmsg,tvtopic,tvtime;
+    private ArrayList<ActivityModel> activityList = new ArrayList<>();
+    Context context;
+
+    public ActivityCardAdapter(ArrayList<ActivityModel> activityList, Context context) {
+        this.activityList = activityList;
+        this.context = context;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tvmsg, tvtopic, tvtime;
         ImageView userprofileimage;
         CardView mCardView;
 
         public MyViewHolder(View itemView) {
             super(itemView);
 
-            this.userprofileimage = (ImageView)itemView.findViewById(R.id.userProfileImage);
-            this.tvmsg = (TextView)itemView.findViewById(R.id.msg);
-            this.tvtopic = (TextView)itemView.findViewById(R.id.topic);
-            this.tvtime = (TextView)itemView.findViewById(R.id.time);
-            this.mCardView = (CardView)itemView.findViewById(R.id.card_activity);
+            this.userprofileimage = (ImageView) itemView.findViewById(R.id.userProfileImage);
+            this.tvmsg = (TextView) itemView.findViewById(R.id.msg);
+            this.tvtopic = (TextView) itemView.findViewById(R.id.topic);
+            this.tvtime = (TextView) itemView.findViewById(R.id.time);
+            this.mCardView = (CardView) itemView.findViewById(R.id.card_activity);
         }
     }
 
 
-    public ActivityCardAdapter(ArrayList<ActivityModel> data1){this.dataset = data1;}
+    public ActivityCardAdapter(ArrayList<ActivityModel> data1) {
+        this.activityList = data1;
+    }
 
     @Override
-    public ActivityCardAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,int viewType){
+    public ActivityCardAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.card_activity, parent, false);
 
         ActivityCardAdapter.MyViewHolder myViewHolder1 = new ActivityCardAdapter.MyViewHolder(view);
         return myViewHolder1;
     }
+
     @Override
-    public void onBindViewHolder(final ActivityCardAdapter.MyViewHolder holder, final int listPosition){
+    public void onBindViewHolder(final ActivityCardAdapter.MyViewHolder holder, final int listPosition) {
         TextView tvmsg = holder.tvmsg;
         TextView tvtopic = holder.tvtopic;
         TextView tvtime = holder.tvtime;
         ImageView userprofileimage = holder.userprofileimage;
         userprofileimage.setImageResource(R.drawable.profile1);
 
-        tvmsg.setText(dataset.get(listPosition).getTvmsg());
-        userprofileimage.setImageResource(dataset.get(listPosition).getUserProfileImage());
-        tvmsg.setText(dataset.get(listPosition).getTvmsg());
-        tvtopic.setText(dataset.get(listPosition).gettvtopic());
-        tvtime.setText(dataset.get(listPosition).getTvtime());
+        ActivityModel am = activityList.get(listPosition);
+        Picasso.with(holder.userprofileimage.getContext()).load(am.getUserImgURL()).into(holder.userprofileimage);
+
+        tvmsg.setText(activityList.get(listPosition).getTvmsg());
+//        userprofileimage.setImageResource(activityList.get(listPosition).getUserImgURL());
+//        Picasso.with(context.getUserImgURL).load(activityList.getString("profile_pick")).into(context.userProfileImage);
+//        c.getString("activity_name"),
+        tvmsg.setText(activityList.get(listPosition).getTvmsg());
+        tvtopic.setText(activityList.get(listPosition).gettvtopic());
+        tvtime.setText(activityList.get(listPosition).getTvtime());
     }
+
     @Override
-    public int getItemCount(){return dataset.size();}
+    public int getItemCount() {
+        return activityList.size();
+    }
 }
