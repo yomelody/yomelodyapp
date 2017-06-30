dc2756696a92280cad840091af8cf715c6c9f6e1
Shubhansh Jaiswal <codingbrains36@gmail.com>
2017-06-30 20:58:14 +0000

    StationActivity.java edited online with Bitbucket

--- app/src/main/java/com/instamelody/instamelody/StationActivity.java
+++ app/src/main/java/com/instamelody/instamelody/StationActivity.java
@@ -8,8 +8,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
