866b7c5d34d0d46ac19666dbb37609570da82a86
CBPC-44\ADMIN <codingbrains23@gmail.com>
2017-07-11 05:18:50 +0530

    Add RMethod java file

---
+++ app/src/main/java/com/instamelody/instamelody/utils/RMethod.java
@@ -0,0 +1,30 @@
+package com.instamelody.instamelody.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by ADMIN on 7/11/2017.
+ */
+
+public class RMethod {
+    public static Date getServerDiffrenceDate(String ServerDate)
+    {
+        String clientDnT = ServerDate ;// "2017-06-01 07:20:00";
+        Date date2=null;
+        SimpleDateFormat dff = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+        try{
+            dff.setTimeZone(TimeZone.getTimeZone("UTC"));
+            date2 = dff.parse(clientDnT);
+            dff.setTimeZone(TimeZone.getDefault());
+            String formattedDate = dff.format(date2);
+        }
+        catch(Exception e){
+            System.err.println(e);
+        }
+        
+        return date2;
+    }
+}
