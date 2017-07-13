f7da471c817d2a57df31c8b18f1cfd5b0f5f6028
Ajay <codingbrains23@gmail.com>
2017-07-13 21:18:53 +0000

    RMethod.java edited online with Bitbucket

--- app/src/main/java/com/instamelody/instamelody/utils/RMethod.java
+++ app/src/main/java/com/instamelody/instamelody/utils/RMethod.java
@@ -20,7 +20,7 @@ public class RMethod {
             date2 = dff.parse(clientDnT);
             dff.setTimeZone(TimeZone.getDefault());
             String formattedDate = dff.format(date2);
-            String tskes;
+            String tss;
         }
         catch(Exception e){
             System.err.println(e);
