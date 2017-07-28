0ef163ef701a75fc522d8427800624ffcd02a359
Saurabh Singh <codingbrains46@gmail.com>
2017-07-28 22:02:11 +0530

    changess

---
+++ app/src/main/java/com/instamelody/instamelody/Adapters/UserMelodyAdapter.java
@@ -0,0 +1,8 @@
+package com.instamelody.instamelody.Adapters;
+
+/**
+ * Created by CBPC 41 on 7/28/2017.
+ */
+
+public class UserMelodyAdapter {
+}
---
+++ app/src/main/java/com/instamelody/instamelody/Models/UserMelodyPlay.java
@@ -0,0 +1,8 @@
+package com.instamelody.instamelody.Models;
+
+/**
+ * Created by CBPC 41 on 7/27/2017.
+ */
+
+public class UserMelodyPlay {
+}
