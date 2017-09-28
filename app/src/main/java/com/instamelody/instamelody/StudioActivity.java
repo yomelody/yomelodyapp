4a34ac195580693b2124069fc8f0bd256161cc62
CBPC-44\ADMIN <codingbrains23@gmail.com>
2017-09-28 22:02:03 +0530

    no message

--- app/src/main/java/com/instamelody/instamelody/StudioActivity.java
+++ app/src/main/java/com/instamelody/instamelody/StudioActivity.java
@@ -70,7 +70,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ByteArrayPool;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareDialog;
 import com.instamelody.instamelody.Adapters.InstrumentListAdapter;
 import com.instamelody.instamelody.Adapters.MelodyCardListAdapter;
@@ -86,6 +91,9 @@ import com.instamelody.instamelody.utils.AppHelper;
 import com.instamelody.instamelody.utils.VolleyMultipartRequest;
 import com.instamelody.instamelody.utils.VolleySingleton;
 import com.squareup.picasso.Picasso;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -99,6 +107,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.SimpleDateFormat;
@@ -110,7 +119,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import io.fabric.sdk.android.Fabric;
 
+import static com.instamelody.instamelody.SignInActivity.TWITTER_CONSUMER_KEY;
+import static com.instamelody.instamelody.SignInActivity.TWITTER_CONSUMER_SECRET;
 import static com.instamelody.instamelody.utils.Const.ServiceType.ADD_RECORDINGS;
 import static com.instamelody.instamelody.utils.Const.ServiceType.AuthenticationKeyName;
 import static com.instamelody.instamelody.utils.Const.ServiceType.AuthenticationKeyValue;
@@ -269,6 +281,8 @@ public class StudioActivity extends AppCompatActivity {
     public static List<MediaPlayer> mp_start = new ArrayList<MediaPlayer>();
     public static final Handler handler = new Handler();
     private String RECORDING_ID = "rid";
+    CallbackManager callbackManager;
+    URL ShortUrl;
     String pos = "0";
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -1815,6 +1829,27 @@ public class StudioActivity extends AppCompatActivity {
                         editorT.putString("thumbnailUrl", "http://52.89.220.199/api/thumbnail_url.php/?cp=http://52.89.220.199/api/uploads/cover.jpg&rc=http://52.89.220.199/api/uploads/recordings/rec1503669372.mp3");
                         editorT.apply();
 
+                        SharedPreferences switchFbEditor = getApplicationContext().getSharedPreferences("SwitchStatus", MODE_PRIVATE);
+                        int switchFbStatus = switchFbEditor.getInt("switch", 0);
+
+                        if (switchFbStatus == 1) {
+                            FbShare();
+                            SharedPreferences.Editor switchFbEditor1 = getApplicationContext().getSharedPreferences("SwitchStatus", MODE_PRIVATE).edit();
+                            switchFbEditor1.clear();
+                            switchFbEditor1.apply();
+                        } else if (switchFbStatus == 2 && switchFbStatus!=1) {
+                            TweetShare();
+                            SharedPreferences.Editor switchFbEditor1 = getApplicationContext().getSharedPreferences("SwitchStatus", MODE_PRIVATE).edit();
+                            switchFbEditor1.clear();
+                            switchFbEditor1.apply();
+                        }else if (switchFbStatus == 3){
+                            FbShare();
+                            TweetShare();
+                            SharedPreferences.Editor switchFbEditor1 = getApplicationContext().getSharedPreferences("SwitchStatus", MODE_PRIVATE).edit();
+                            switchFbEditor1.clear();
+                            switchFbEditor1.apply();
+                        }
+
 
                     }
 
@@ -2500,6 +2535,73 @@ public class StudioActivity extends AppCompatActivity {
         } else {
             melody_detail.setText("" + instCount + " Instrumentals");
         }
+    }
+    public void FbShare() {
+        SharedPreferences editorT = getApplicationContext().getSharedPreferences("thumbnail_url", MODE_PRIVATE);
+        String fetchThumbNailUrl = editorT.getString("thumbnailUrl", null);
+
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+        // this part is optional
+        shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+            @Override
+            public void onSuccess(Sharer.Result result) {
+                Toast.makeText(StudioActivity.this, "Recording Uploaded", Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editorT = getApplicationContext().getSharedPreferences("thumbnail_url", MODE_PRIVATE).edit();
+                editorT.clear();
+                editorT.apply();
+            }
+
+            @Override
+            public void onCancel() {
+
+                Toast.makeText(StudioActivity.this, "Recording not Uploaded", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+
+        });
+
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                    .setContentUrl(Uri.parse(fetchThumbNailUrl))
+//                    .setImageUrl(Uri.parse(cover))
+                    .build();
+            shareDialog.show(linkContent, ShareDialog.Mode.FEED);
+        }
+    }
+
+    public void TweetShare() {
+        SharedPreferences editorT = getApplicationContext().getSharedPreferences("thumbnail_url", MODE_PRIVATE);
+        String fetchThumbNailUrl = editorT.getString("thumbnailUrl", null);
+
+        try {
+            ShortUrl = new URL(fetchThumbNailUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new TweetComposer());
+
+        /*Bundle bundle = getIntent().getExtras().getBundle(SHARE_DATA);
+        String description = bundle.getString(SHARE_DESCRIPTION);
+        String title = bundle.getString(SHARE_TITLE);
+        String picture = bundle.getString(SHARE_PICTURE_LINK);
+        String link = bundle.getString(SHARE_LINK);*/
+
+        TweetComposer.Builder builder = null;
+
+
+        builder = new TweetComposer.Builder(this)
+//                    .text(title + "" + description)
+                .text("Audio Url")
+                .url(ShortUrl);
+//                .image(Uri.parse(cover));
+        builder.show();
     }
 }
 
