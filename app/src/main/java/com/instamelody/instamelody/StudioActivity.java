08da2c5cce536ac1539dc69d24602129de13656a
Abhishek <codingbrains41@gmail.com>
2017-10-03 23:26:52 +0530

    Abhishek Changes 03-10-2017

--- app/src/main/java/com/instamelody/instamelody/StudioActivity.java
+++ app/src/main/java/com/instamelody/instamelody/StudioActivity.java
@@ -290,6 +290,7 @@ public class StudioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studio);
+        progressDialog = new ProgressDialog(StudioActivity.this);
 //        instruments_count = InstrumentListAdapter.instruments_url;
 //        Log.d("abc", "" + instruments_count.size());
         rlBase = (RelativeLayout) findViewById(R.id.rlBase);
@@ -1716,6 +1717,10 @@ public class StudioActivity extends AppCompatActivity {
     }
 
     public void uploadRecordingsMixing(final String id) {
+        progressDialog.setTitle("Processing...");
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
 
         VolleyMultipartRequest multipartRequest = new VolleyMultipartRequest(Request.Method.POST, MixingAudio_InstrumentsAudio, new Response.Listener<NetworkResponse>() {
             @Override
